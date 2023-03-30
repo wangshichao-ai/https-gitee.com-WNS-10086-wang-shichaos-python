e1867d5d9ebf876a57312a5b20a079b0d86eb387
王世超 <12527855+WNS-10086@user.noreply.gitee.com>
2023-03-30 08:32:19 +0000

    2组王世超
    Signed-off-by: 王世超 <12527855+WNS-10086@user.noreply.gitee.com>

---
+++ 第一章练习.py
@@ -0,0 +1,48 @@
+z=2+5
+type (z)
+print(z.conjugate())
+###
+
+pow(3,60)
+###
+
+import sys
+print (sys.float_info)
+print(sys.int_info)
+###
+
+tup1=(1,2,3,4,5)
+number=(123456)
+list1=[12,3,4,5]
+dict ={'name':'I love You','age':19,'mail':'2216238538@qq.com'}
+basket={'apple','banana','pear','orange'}
+print(tup1)
+print (number)
+print(list1)
+print(dict)
+print(basket)
+###
+
+a = b = 250
+c=a+b
+print("a+b=", c)
+c=a*b
+print("a*b=", c)
+a=520
+b=20
+c=a/b
+print("a/b=", c)
+c=a//b
+print("a//b=", c)
+c = a%b
+print("a%b =", c)
+c = a ** b
+print("a**b=", c)
+###
+
+var1 = 'I like you'
+var2 = "i will go where no one goes"
+var3='''do you know that i will love you forever '''
+print(var1)
+print (var2)
+print(var3)
