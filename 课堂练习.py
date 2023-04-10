65d1b4d10831832ea42ae6a207f515a8f45d52e9
Your Namegit config --global user.email <2216238538@qq.com git config --global user.email>
2023-04-10 16:16:52 +0800

    ok

---
+++ 课堂练习.py
@@ -0,0 +1,11 @@
+I = int(input("please input: "))
+if(I>=90):
+    print("a")
+if(80<=I<90):
+    print("b")
+if(70<=I<80):
+    print("c")
+if(60<=I<70):
+    print("d")
+if(I<60):
+    print("e")
---
+++ 课堂练习2.py
@@ -0,0 +1,8 @@
+x = int(input("please input: "))
+if(x>=90):
+    print("A")
+elif(x>=80):
+    print("B")
+elif(x>=70):
+    print("C")
+    
