9811043d5b45515dd74a607b0226689eacb6a00c
王世超 <12527855+WNS-10086@user.noreply.gitee.com>
2023-04-12 07:48:08 +0000

    二组王世超
    Signed-off-by: 王世超 <12527855+WNS-10086@user.noreply.gitee.com>

---
+++ test2.java
@@ -0,0 +1,10 @@
+public class test2 {
+    public static void main(String[] args) {
+        char[] c = { 'a', 'b', 'c', 'd' };
+        char d = 'i';
+        System.out.println("String is: " + String.valueOf(c));
+        System.out.println("String is: " + String.valueOf(d));
+        System.out.println("String is: " + Character.toString(d));
+    }
+
+}
