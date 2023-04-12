65a9e7715befcb9c626888e559862e8a8f39ec86
王世超 <12527855+WNS-10086@user.noreply.gitee.com>
2023-04-12 07:46:50 +0000

    二组王世超
    Signed-off-by: 王世超 <12527855+WNS-10086@user.noreply.gitee.com>

---
+++ test.java
@@ -0,0 +1,39 @@
+
+public class test {
+    public static void main(String[] args) {
+        int sum1 = 100 + 50;
+        int sum2 = sum1 + 250;
+        int sum3 = sum2 + sum2;
+        System.out.println(sum1);
+        System.out.println(sum2);
+        System.out.println(sum3);
+
+        String txt = "I love three thousand times";
+        System.out.println("The length of the txt string is: " + txt.length());
+        String txt1 = "i will go where no one goes";
+        System.out.println(txt1.toUpperCase());
+
+        int x = 520;
+        int y = 1314;
+        int z = x + y;
+        System.out.println(z);
+
+        String a = "I love three thousand times";
+        String b = ",my Argentina girl";
+        String c = a + b;
+        System.out.println(c);
+
+        int d = Math.max(50, (Math.min(60, 20)));
+        System.out.println(d);
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+
+        int n = 6;
+        int m = 9;
+        System.out.println(n > m);
+
+    }
+}
